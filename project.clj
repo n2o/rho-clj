b7f12d4c1ff7a0791b54cca82418895a4fc4673c
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-02-26 13:55:40 +0100

    Tidy up, add lein ancient

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
 (defproject rho "0.1.0"
-            :description "Simple static site generator"
-            :url "https://github.com/lacarmen/cryogen"
-            :license {:name "Eclipse Public License"
-                      :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.8.0"]
-                           [ring/ring-devel "1.5.1"]
-                           [compojure "1.5.2"]
-                           [ring-server "0.4.0"]
-                           [cryogen-markdown "0.1.6"]
-                           [cryogen-core "0.1.52"]]
-            :plugins [[lein-ring "0.9.7"]]
-            :main rho.core
-            :ring {:init rho.server/init
-                   :handler rho.server/handler})
+  :description "Static website for renate-horsch.de"
+  :url "http://renate-horsch.de"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [ring/ring-devel "1.5.1"]
+                 [compojure "1.5.2"]
+                 [ring-server "0.4.0"]
+                 [cryogen-markdown "0.1.6"]
+                 [cryogen-core "0.1.52"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-ancient "0.6.10"]]
+  :main rho.core
+  :ring {:init rho.server/init
+         :handler rho.server/handler})
